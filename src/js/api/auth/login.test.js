224aada33947d98a0a680288572600c47d45dd45
Sirvau <sirols50984@stud.noroff.no>
2024-04-11 20:48:49 +0200

    Defined localStorageMock-variable and commented import to login test

---
+++ src/js/api/auth/login.test.js
@@ -0,0 +1,2 @@
+//import localStorageMock from "../../mocks/localStorage.mock.js";
+//import login from "./login.js";
---
+++ src/js/mocks/localStorage.mock.js
@@ -0,0 +1,6 @@
+export const localStorageMock = {
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+  clear: jest.fn(),
+  removeItem: jest.fn(),
+};
