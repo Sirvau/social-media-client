19b1ddeeb60e223fd36f7a1b2ce64d2455b1ddeb
Sirvau <sirols50984@stud.noroff.no>
2024-04-12 21:55:48 +0200

    Added unit test for log in

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,1 +1,32 @@
-//import * as mocks from "../../mocks/index.mock.js";
+import { login } from "./login";
+import { fetchMock, save } from "../../mocks/index.mocks.js";
+
+const profile = {
+  name: "username",
+  email: "example@noroff.no",
+  password: "myPassword",
+  avatar: "avatarURL",
+};
+
+global.fetch = fetchMock;
+
+describe("login", () => {
+  afterEach(() => {
+    fetchMock.mockClear();
+  });
+
+  it("logs in and saves token", async () => {
+    await login(profile);
+    expect(save).toHaveBeenNthCalledWith(1, "token", JSON.stringify("token"));
+  });
+
+  it("Throws an error when response is not ok", async () => {
+    const errorMessage = "Unauthorized";
+
+    fetchMock.mockResolvedValueOnce({
+      ok: false,
+      statusText: errorMessage,
+    });
+    await expect(login(profile)).rejects.toThrow(errorMessage);
+  });
+});
