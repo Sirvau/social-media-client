11aaed3325225c2974b0e72cccc95ce999a7801c
Sirvau <sirols50984@stud.noroff.no>
2024-04-14 20:43:35 +0200

    Login-test success

--- cypress/e2e/test/login.cy.js
+++ cypress/e2e/test/login.cy.js
@@ -1,17 +1,11 @@
-import {
-  validEmail,
-  ivalidEmail,
-  validPassword,
-  invalidPassword,
-} from "../../support/test-user-data.js";
+import { validEmail, validPassword } from "../../support/test-user-data.js";
 
 // from the CA requirements:
 // The user can log in with the login form with valid credentials
-// The user cannot submit the login form with invalid credentials and is shown a message.
 
-describe("Navigates to homepage", () => {
+describe("Logs in with valid creditentials", () => {
   beforeEach(() => {
-    cy.visit("https://sirvau.github.io/social-media-client/");
+    cy.visit("/");
     cy.wait(1000);
   });
 
@@ -19,5 +13,17 @@ describe("Navigates to homepage", () => {
     cy.get("#registerForm").find("button[data-auth=login]").click();
     cy.wait(1000);
     cy.get("#loginForm").should("be.visible");
+    cy.wait(1000);
+  });
+
+  it("Fills out login-form", () => {
+    cy.get('.modal-footer button[data-auth="login"]')
+      .should("be.visible")
+      .click();
+    cy.wait(500);
+    cy.get("#loginEmail").type(validEmail, { delay: 50 });
+    cy.get("#loginPassword").type(validPassword, { delay: 50 });
+    cy.wait(500);
+    cy.contains('button[type="submit"]', "Login").click();
   });
 });
