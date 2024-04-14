cf1d17b0a42bb3a386bed0f6b106253f9020d89e
Sirvau <sirols50984@stud.noroff.no>
2024-04-14 17:25:40 +0200

    Added user variables

---
+++ cypress/support/test-user-data.js
@@ -0,0 +1,7 @@
+//User data for tests
+
+export const validEmail = "valid_email";
+export const validPassword = "valid_password";
+
+export const invalidEmail = "valid_user";
+export const invalidPassword = "invalid_password";
